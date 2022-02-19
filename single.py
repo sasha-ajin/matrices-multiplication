e3b29393baf98a2bc5c6ad4f75a275a1562a2b6e
Sasha Ajin <sashaajin@icloud.com>
2022-02-19 23:14:08 +0200

    edit comments in python

--- single.py
+++ single.py
@@ -6,11 +6,11 @@ def multiplication_matrices(matrix1, matrix2):
     result_width = len(matrix2[0])
     result_height = len(matrix1)
     result = [[0 for width in range(result_width)] for height in range(result_height)]
-    # iterate through rows of matrix1
+    """iterate through rows of matrix1 """
     for i in range(len(matrix1)):
-        # iterate through columns of matrix2
+        """iterate through columns of matrix2"""
         for j in range(len(matrix2[0])):
-            # iterate through rows of matrix2
+            """iterate through rows of matrix2"""
             for k in range(len(matrix2)):
                 result[i][j] += matrix1[i][k] * matrix2[k][j]
     return result
