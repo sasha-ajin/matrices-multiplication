af9f66417424b5749e4e11a12a7b8243a0e45cb9
Sasha Ajin <sashaajin@icloud.com>
2022-03-19 00:08:39 +0200

    add parallel item multiplication

--- multi_process_solution.py
+++ multi_process_solution.py
@@ -1,21 +1,40 @@
-from multiprocessing import Manager, Process
+from audioop import mul
+from multiprocessing import Manager, Process, Queue
 import random
+import time
 
 
 def multiplication_matrices(matrix1, matrix2):
+    def item_calculation(index, mul1, mul2, queue):
+        queue.put({"index": index, "result": mul1 * mul2})
+
     def result_row_calculation(
         result,
         row_index,
         matrix_2_columns_quanity,
         matrix_2_rows_quanity,
     ):
+        queue = Queue()
         result_row = result[row_index]
+        item_calculation_processes = []
         for matrix2_column_index in range(matrix_2_columns_quanity):
             for matrix2_row_index in range(matrix_2_rows_quanity):
-                result_row[matrix2_column_index] += (
-                    matrix1[matrix1_row_index][matrix2_row_index]
-                    * matrix2[matrix2_row_index][matrix2_column_index]
+                item_calculation_process = Process(
+                    target=item_calculation,
+                    args=(
+                        matrix2_column_index,
+                        matrix1[matrix1_row_index][matrix2_row_index],
+                        matrix2[matrix2_row_index][matrix2_column_index],
+                        queue,
+                    ),
                 )
+                item_calculation_process.start()
+                item_calculation_processes.append(item_calculation_process)
+        for item_calculation_process in item_calculation_processes:
+            item_calculation_process.join()
+        while not queue.empty():
+            item = queue.get()
+            result_row[item["index"]] += item["result"]
         result[row_index] = result_row
 
     matrix_1_columns_quanity, matrix_1_rows_quanity = len(matrix1[0]), len(matrix1)
@@ -49,18 +68,4 @@ if __name__ == "__main__":
 
     """Second matrix, we will multiplicate"""
     matrix2 = [[random.randrange(1, 10) for width in range(3)] for height in range(3)]
-    five_to_five_1 = [
-        [2, 2, 4, 8, 9],
-        [1, 9, 9, 5, 6],
-        [8, 5, 3, 7, 8],
-        [4, 9, 3, 6, 2],
-        [4, 1, 4, 6, 3],
-    ]
-    five_to_five_2 = [
-        [7, 6, 3, 4, 5],
-        [2, 8, 4, 3, 5],
-        [8, 1, 4, 7, 6],
-        [3, 6, 5, 6, 1],
-        [5, 3, 3, 4, 2],
-    ]
-    multiplication_matrices(five_to_five_1, five_to_five_2)
+    multiplication_matrices(matrix1, matrix2)
